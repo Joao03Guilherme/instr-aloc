d8eb035d260f8ddd79bd3b520468ba11083bfd88
Joao03Guilherme <joaommguilherme@gmail.com>
2022-09-01 18:52:47 +0100

    Updated input file format from xlsx to csv

--- Algoritmo_Geral/instr_aloc_flow.py
+++ Algoritmo_Geral/instr_aloc_flow.py
@@ -1,13 +1,14 @@
-from argparse import ArgumentError
 from network_flow_algorithm import FastMinCostNetworkFlowSolver
 import pandas as pd 
 import os
 
+# TODO: REFACTOR CODE, CHANGE VARIABLE NAMES, SET OUTPUT TO CSV
+
 INF = 999
 
-INPUT_OUTPUT_FILENAME="data.xlsx"
-OUTPUT_FILENAME = "output.xlsx"
-INPUT_COURSE_DATA_FILENAME = "course_data.xlsx"
+STAFF_FORM_FILENAME ="data.csv"
+OUTPUT_FILENAME = "output.csv"
+COURSE_DATA_FILENAME = "course_data.csv"
 
 # Changing python running directory
 abspath = os.path.abspath(__file__)
@@ -15,92 +16,110 @@ dname = os.path.dirname(abspath)
 os.chdir(dname)
 
 class Instructor:
-    def __init__(self, name, id, email):
-        self.name = name
-        self.id = id
+    def __init__(self, email, id):
         self.email = email
         self.allocated = False
         self.course = None 
+        self.possible_months = None
+        self.id = id
         
 class Course:
     def __init__(self, name, id, min_staff, max_staff):
         self.name = name
         self.min_staff = min_staff
         self.max_staff = max_staff
-        self.id = id
         self.allocated_instructors = []
         self.can_open = True
+        self.id = id
+
+class Preference:
+    def __init__(self, instructor, course, preference_value : int, possible_months):
+        self.instructor = instructor
+        self.course = course
+        self.preference_value = preference_value
+        self.possible_months = possible_months
+
+def get_course_or_instructor_by_id(course_or_instructor_list, id):
+    for course_or_instructor in course_or_instructor_list:
+        if course_or_instructor.id == id:
+            return course_or_instructor 
+
+def get_course_by_name(course_list, name):
+    for course in course_list:
+        if course.name == name:
+            return course
+
+def get_instructor_by_email(instructor_list, email):
+    for instructor in instructor_list:
+        if instructor.email == email:
+            return instructor
 
 def import_data():
-    """Import data.
-    It expects two excel files:
-        Staff Form data: input data to have 5 initial columns (index, timestamp, name, email, phone, availability) and then all the preferences.
-        Course Staff needs: input data to have 3 rows: course name, minimum staff and maximum staff.
+    """Imports data from csv to python.
+    It expects two csv files:
+        Staff Form data: input data to have 4 columns (email, interest, name, possible_months) 
+        Course Staff needs: input data to have 3 columns: course name, minimum staff and maximum staff.
     Returns:
-        instructors (pd.DataFrame): instrutors name and email.
-        courses (pd.DataFrame): list of courses.
-        preferences (pd.DataFrame): instrutors' preferences.
+        instructor_emails (np.ndarray): instrutors email.
+        interests (dict): maps instructor email to list of interests in courses
+        staff_per_course (dict): maps course_name to staff requirements (min and max)
     """
-    df_staff_form = pd.read_excel(INPUT_OUTPUT_FILENAME) 
-    df_course_needs = pd.read_excel(INPUT_COURSE_DATA_FILENAME)
+    df_staff_form = pd.read_csv(STAFF_FORM_FILENAME) 
+    df_course_data = pd.read_csv(COURSE_DATA_FILENAME)
 
-    preferences = df_staff_form.iloc[:, 6:].fillna(-1).values.astype(int)  # [X:-X] depends on the Excel format
-    instructors = df_staff_form[["Nome","Email"]].values
-    open_course_names = df_staff_form.columns[6:].values
+    instructor_emails = set(df_staff_form["email"].values)
+    open_course_names = set(df_staff_form["name"].values)
+    interests = {email : [] for email in instructor_emails}
+    for email, interest, name, possible_months in df_staff_form.values:
+        possible_months_list = possible_months.strip("}{").split(",")
+        biased_interest = 10 ** int(interest) + len(possible_months_list) # Give more value to preference that has more months
+        interests[email].append((name, biased_interest, possible_months_list))
     
-    course_names = df_course_needs.columns[1:].values
-    course_min_staff = df_course_needs.iloc[0, 1:].values.astype(int)
-    course_max_staff = df_course_needs.iloc[1, 1:].values.astype(int)
 
+    course_names = df_course_data["course"].values
+    course_min_staff = df_course_data["min_staff"].values.astype(int)
+    course_max_staff = df_course_data["max_staff"].values.astype(int)
     # Load minimum and maximum staff per course
     staff_per_course = {}
     for i, course_name in enumerate(course_names):
-        if course_min_staff[i] > course_max_staff[i]:
-            raise ArgumentError # Min Staff must not be greater than max staff
+        if course_name in open_course_names: staff_per_course[course_name] = (int(course_min_staff[i]), int(course_max_staff[i]))
 
-        staff_per_course[course_name] = (int(course_min_staff[i]), int(course_max_staff[i])) 
-        
-    # Get data only for open courses
-    staff_per_open_course = {}
     for course_name in open_course_names:
-        staff_per_open_course[course_name] = staff_per_course[course_name]
+        if course_name not in staff_per_course:
+            raise KeyError(f"O curso '{course_name}' não tem os requisitos definidos no ficheiro '{COURSE_DATA_FILENAME}'")
 
-    return instructors, staff_per_open_course, preferences
+    return instructor_emails, interests, staff_per_course
 
 def output_data(course_list, instr_list):
-    """Outputs data to a 'beautiful' Excel file.
+    """Outputs result to a csv file.
     Args:
-        course_list (list): List of courses, each course is an object.
-        instr_list (list): List of instructors.
-        course (object): Course information.
-        instructor (object): Instructor information.
+        course_list (list): List of Course objects.
+        instr_list (list): List of Instructor objects.
     """
     output_data = []
     for course in course_list:
         if course.can_open:
             for instructor in course.allocated_instructors:
-                output_data.append([course.name, instructor.name, instructor.email])
+                output_data.append([course.name, instructor.email, instructor.possible_months])
 
     for instructor in instr_list:
         if not instructor.allocated:
             output_data.append(["Sem curso", instructor.name, instructor.email])
     
     df = pd.DataFrame(output_data)
-    df.columns = ["Curso", "Nome", "Email"]
-    df.to_excel(OUTPUT_FILENAME)
+    df.columns = ["Course", "Email", "Possible Months"]
+    df.to_csv(OUTPUT_FILENAME, sep=",", index=False)
     
-
-def build_bipartite_graph(instr_list, course_list, preferences, id_to_course_or_instructor, tight):
-    """Returns a flow network with instructors and courses
+def build_bipartite_graph(instructor_list, course_list, preferences, tight):
+    """Returns a flow network with instructors and courses as vertices
     
     Args:
-        instr_list (list): List of instructors, each instructor is an object
-        course_list (list): List of courses, each course is an object.
-        preferences (list): List of lists, preferences[i][j] represents preference of ith instructor for the jth course
-        id_to_course_or_instructor (dict): maps vertex id to instructor or course
+        instructor_list (list): List of Instructor objects
+        course_list (list): List of Course objects
+        preferences (dict): Maps email to list of Preference objects 
         tight (bool): True if the algorithm is trying to guarantee the minimum number of instructors per course
     """
-    n = len(instr_list) + len(course_list) + 2 # Add two vertices for source and sink
+    n = len(instructor_list) + len(course_list) + 2 # Add two vertices for source and sink
     s, t = n - 1, n - 2
     edges = []
 
@@ -110,13 +129,12 @@ def build_bipartite_graph(instr_list, course_list, preferences, id_to_course_or_
             min_staff = course.min_staff - n_of_allocated_instructors if tight else course.max_staff - n_of_allocated_instructors
             edges.append((course.id, t, min_staff, 0)) # Add edge from course to sink
     
-    for i, instructor in enumerate(instr_list):    
+    for instructor in instructor_list:    
         if not instructor.allocated:
             edges.append((s, instructor.id, 1, 0)) # Add edge from source to instructor
-            for j, preference in enumerate(preferences[i]):
-                course = id_to_course_or_instructor[j]
-                if course.can_open and preference != -1:
-                    edges.append((instructor.id, course.id, 1, int(2 - preference))) # Minimize cost 
+            for preference in preferences[instructor.email]:
+                if preference.course.can_open:
+                    edges.append((instructor.id, preference.course.id, 1, int(INF - preference.preference_value))) # Minimize cost 
 
     solver = FastMinCostNetworkFlowSolver(n, s, t)
     for edge in edges:
@@ -124,49 +142,56 @@ def build_bipartite_graph(instr_list, course_list, preferences, id_to_course_or_
 
     return solver
 
-def run_solver(instr_list, course_list, preferences, id_to_course_or_instructor, tight):
-    """Runs the allocation algorithm
-
+def run_solver(instructor_list, course_list, preferences, tight):
+    """Runs the graph-flow allocation algorithm
     Args:
-        instr_list (list): List of instructor objects
-        course_list (list): List of courses, each course is an object.
-        preferences (list): List of lists, preferences[i][j] represents preference of ith instructor for the jth course
-        id_to_course_or_instructor (dict): maps vertex id to instructor or course
+        instructor_list (list): List of Instructor objects
+        course_list (list): List of Course objects
+        preferences (dict): Maps email to list of Preference objects 
         tight (bool): True if the algorithm is trying to guarantee the minimum number of instructors per course
     """
-    solver = build_bipartite_graph(instr_list, course_list, preferences, id_to_course_or_instructor, tight)
+    solver = build_bipartite_graph(instructor_list, course_list, preferences, tight)
     graph = solver.get_graph()
     
-    for i in range(len(course_list), len(course_list) + len(instr_list)):
-        instructor = id_to_course_or_instructor[i] 
-        for edge in graph[i]:
+    for instructor in instructor_list: 
+        for edge in graph[instructor.id]:
             if edge.flow > 0:
-                course = id_to_course_or_instructor[edge.end]
-                
-                instructor.allocated = True
-                instructor.course = course 
-                course.allocated_instructors.append(instructor)
+                course = get_course_or_instructor_by_id(course_list, edge.end)
+                for preference in preferences[instructor.email]:
+                    if preference.course == course:
+                        instructor.allocated = True
+                        instructor.course = course 
+                        instructor.possible_months = preference.possible_months
+                        course.allocated_instructors.append(instructor)
+                        break
                 break
      
 def main():
-    instructors, staff_per_course, preferences = import_data()
-    instructor_list, course_list, id_to_course_or_instructor = [], [], {}
+    instructor_emails, interests, staff_per_course = import_data()
+    instructor_list, course_list, preferences = [], [], {email : [] for email in instructor_emails}
     
-    id = 0 
-    for name, (min_staff, max_staff) in staff_per_course.items():
-        course = Course(name, id, min_staff, max_staff)
-        id_to_course_or_instructor[id] = course
+    id = 0
+    for course_name, (min_staff, max_staff) in staff_per_course.items():
+        course = Course(course_name, id, min_staff, max_staff)
         course_list.append(course)
         id += 1
     
-    for (name, email) in instructors:
-        instructor = Instructor(name, id, email)
-        id_to_course_or_instructor[id] = instructor         
-        instructor_list.append(instructor)   
-        id += 1
+    for email in instructor_emails:
+        instructor = Instructor(email, id)
+        instructor_list.append(instructor)
+        id += 1   
    
+    for instructor_email, interest_list in interests.items():
+        for interest in interest_list:
+            instructor = get_instructor_by_email(instructor_list, instructor_email)
+            course = get_course_by_name(course_list, interest[0])
+            preference_value : int = interest[1]
+            possible_months : list = interest[2]
+            preferences[instructor_email].append(Preference(instructor, course, preference_value, possible_months))
+        
+    
     # First pass is to guarantee minimum number of instructors
-    run_solver(instructor_list, course_list, preferences, id_to_course_or_instructor, True)
+    run_solver(instructor_list, course_list, preferences,  True)
 
     # Iteratively remove courses with not enough instructors and re-run the algorithm
     for course in course_list:
@@ -176,10 +201,10 @@ def main():
                 instructor.allocated = False 
                 instructor.course = None
 
-            run_solver(instructor_list, course_list, preferences, id_to_course_or_instructor, True)
+            run_solver(instructor_list, course_list, preferences, True)
 
     # Last run to allocate remaining instructors
-    run_solver(instructor_list, course_list, preferences, id_to_course_or_instructor, False)
+    run_solver(instructor_list, course_list, preferences, False)
     output_data(course_list, instructor_list)
                   
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ O respositório contém duas versões do algoritmo.
 
 ## Como executar?
 * Obter os ficheiros com: `git clone https://github.com/Joao03Guilherme/instr-aloc`
-* Na pasta _Algoritmo\_Geral_ inserir os dados nos dois ficheiros Excel com a input para o algoritmo:
-  * `data.xlsx`: as primeiras 4 colunas são "Timestamp", "Nome", "Email", "Contacto telefónico" e "Em que meses esperas estar disponível para participar?", as restantes colunas são referentes aos cursos.
-  * `course_data.xlsx`: contém dados referentes às necessidades de staff para cada curso (staff mínimo e máximo). É composto por três linhas: "Curso", "Staff Mínimo" e "Staff Máximo".
+* Na pasta _Algoritmo\_Geral_ inserir os dados nos dois ficheiros csv com a input para o algoritmo:
+  * `data.csv`: as colunas são "email", "interest", "name" e "possible_months"
+  * `course_data.csv`: contém dados referentes às necessidades de staff para cada curso (staff mínimo e máximo). É composto por três colunas: "course", "min_staff" e "max_staff".
 * Executar o ficheiro `instr_aloc_flow.py`.
